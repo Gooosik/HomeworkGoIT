c92151457319aadb3b5f38484854f9d79822663f
Gooosik <163407030+Gooosik@users.noreply.github.com>
2024-10-23 22:50:51 +0300

    normalize_phone_number.py

--- 1work/3.py
+++ 1work/3.py
@@ -1,16 +1,15 @@
 import re
 
 def normalize_phone(phone_number):
-
     digits = re.findall(r'\d+', phone_number)
     
     normalized_number = ''.join(digits)
     
     if normalized_number.startswith("0"):
-        normalized_number = "38" + normalized_number
+        normalized_number = "+38" + normalized_number
     elif normalized_number.startswith("380"):
         normalized_number = "+" + normalized_number
-    else:
+    elif not normalized_number.startswith("+380"):
         normalized_number = "+380" + normalized_number
     
     return normalized_number
