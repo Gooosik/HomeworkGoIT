0a7bb33442438607b84935e02f5a0adf155f3dfb
Gooosik <163407030+Gooosik@users.noreply.github.com>
2024-10-23 22:41:48 +0300

    get_days_from_today.py

--- 1work/1.py
+++ 1work/1.py
@@ -2,12 +2,16 @@ from datetime import datetime
 
 def get_days_from_today(date):
 
-    input_date = datetime.strptime(date, '%Y-%m-%d').date()
+    try:
 
-    date_now = datetime.today().date()
+        input_date = datetime.strptime(date, '%Y-%m-%d').date()
+        date_now = datetime.today().date()
+        result = (date_now - input_date).days
 
-    result = (date_now - input_date).days
-
-    return result
+        return result
+    
+    except ValueError:
+        
+        return "Неправильний формат дати"
 
 print(get_days_from_today('2006-06-03'))
